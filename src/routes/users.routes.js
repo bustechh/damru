f06cd32199d991cdef1a60b6d2a4e8c454e25be3
bustechh <techbushelp@gmail.com>
2025-06-20 15:30:43 +0530

    Create users.routes.js

---
+++ src/routes/users.routes.js
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage} from '../controllers/users.controllers.js';
+import {upload} from '../middlewares/multer.middlewares.js';
+import {verifyJWT} from '../middlewares/auth.middlewares.js';
+
+const router = Router()
+
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ]), 
+    registerUser
+)
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT, logoutUser)
+
+router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+
+export default router
